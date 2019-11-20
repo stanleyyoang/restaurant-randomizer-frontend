23fb51aa5c63039f52253f7c65cb9be4ff6d4982
Stanley Yoang <stanleyyoang@gmail.com>
2019-11-19 19:10:47 -0600

    added double ended slider, currently on the wrong page tho
    documentation: https://github.com/syedowaisali/crystal-range-seekbar

--- app/src/main/java/com/example/restaurantrandomizer/MainActivity.java
+++ app/src/main/java/com/example/restaurantrandomizer/MainActivity.java
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarFinalValueListener;
+import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,6 +34,30 @@ public class MainActivity extends AppCompatActivity {
 
         cuisine = (Button) findViewById(R.id.btnCuisine);
         cuisineSelected = (TextView) findViewById(R.id.tvCuisineSelected);
+
+        // get seekbar from view
+        final CrystalRangeSeekbar rangeSeekbar = (CrystalRangeSeekbar) findViewById(R.id.rangeSeekbar1);
+
+        // get min and max text view
+        final TextView tvMin = (TextView) findViewById(R.id.textMin1);
+        final TextView tvMax = (TextView) findViewById(R.id.textMax1);
+
+        // set listener
+        rangeSeekbar.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
+            @Override
+            public void valueChanged(Number minValue, Number maxValue) {
+                tvMin.setText(String.valueOf(minValue));
+                tvMax.setText(String.valueOf(maxValue));
+            }
+        });
+
+        // set final value listener
+        rangeSeekbar.setOnRangeSeekbarFinalValueListener(new OnRangeSeekbarFinalValueListener() {
+            @Override
+            public void finalValue(Number minValue, Number maxValue) {
+                Log.d("CRS=>", String.valueOf(minValue) + " : " + String.valueOf(maxValue));
+            }
+        });
 
         listCuisines = getResources().getStringArray(R.array.cuisines);
         checkCuisines = new boolean[listCuisines.length];
